c9dcdcb8521948e63d17769aab99c31ee3bd90dc
Snegirev Anfim Alexandrovich <anfim.sn@gmail.com>
2022-05-16 17:35:23 +0300

    F5 loader fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import {Routes, Route} from 'react-router-dom';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import 'App.css';
 
@@ -7,27 +7,29 @@ import HomePage from 'pages/HomePage';
 import LoginPage from 'pages/LoginPage';
 import RegisterPage from 'pages/RegisterPage';
 import NotFoundPage from 'pages/NotFoundPage';
-import { RootState } from 'store';
+import { AppDispatch, RootState } from 'store';
+import { useDispatch } from 'react-redux';
+import { toggleLoader } from 'store/slices/userSlice';
 
 function App() {
-  const [isLoad, setIsLoad] = useState(false);
+  const dispatch: AppDispatch = useDispatch();
+
   const isLoadStore: boolean = useSelector((state: RootState) => state.user.isLoad);
   
   useEffect(() => {
-    setIsLoad(false);
-  }, []);
-  
-  useEffect(() => {
-    setIsLoad(isLoadStore);
-  }, [setIsLoad, isLoadStore]);
-
+    dispatch(
+      toggleLoader({
+        isLoad: false,
+      }),
+    );
+  }, [dispatch]);
 
   return (
     <>
       <header>
         <section><h1>TakeOff Stuff <span>phonebook</span></h1></section>
       </header>
-      <main className={isLoad ? 'loader': ''}>
+      <main className={isLoadStore ? 'loader': ''}>
         <Routes>
           <Route path='/' element={<HomePage />} />
           <Route path='/login' element={<LoginPage />} />
